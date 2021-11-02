60506336e71ea9829ea16d93c19319f6c1889134
Jonathan Kart <jkart@lightstep.com>
2021-11-02 13:23:28 -0700

    add counters to optionally ignore resourceenrichment resources during upgrade from cwingest

--- resource-enrichment.tf
+++ resource-enrichment.tf
@@ -4,9 +4,14 @@
 # configures the role so that it can be assumed by Lightstep.
 # It creates and attaches the policy LightstepAWSIntegrationPolicy, which allows
 # reading of EC2 instance data and CloudWatch metrics.
+#
+# This file does not need to run if the customer has previously set up Lightstep's AWS integration 
+# prior to the release of the Metric Streams integration. 
+# Please set the `upgrade_to_streams` variable to `true` before applying this terraform.
 
 
 resource "aws_iam_role" "lightstep_role" {
+  count       = var.upgrade_to_streams ? 0 : 1
   name        = "LightstepAWSIntegrationRole"
   description = "Role that Lightstep will assume as part of CloudWatch integration"
 
@@ -32,6 +37,7 @@ EOF
 }
 
 resource "aws_iam_policy" "lightstep_policy" {
+  count       = var.upgrade_to_streams ? 0 : 1
   name        = "LightstepAWSIntegrationPolicy"
   description = "Policy associated with LightstepAWSIntegrationRole"
 
@@ -54,6 +60,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lightstep_attach" {
-  role       = aws_iam_role.lightstep_role.name
-  policy_arn = aws_iam_policy.lightstep_policy.arn
+  count      = var.upgrade_to_streams ? 0 : 1
+  role       = aws_iam_role.lightstep_role[0].name
+  policy_arn = aws_iam_policy.lightstep_policy[0].arn
 }
