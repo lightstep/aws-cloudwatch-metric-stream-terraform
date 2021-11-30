275a10ce3a585ff1037b86e566c1a3a766229612
Jonathan Kart <jkart@lightstep.com>
2021-11-30 11:35:56 -0800

    remove forceps and update comment to reflect that external_id can be any valid string

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "lightstep_access_token" {
 }
 
 variable "external_id" {
-  description = "A randomly generated alphabetical string used to autheticate between Lightstep and your AWS account. Default: <empty>"
+  description = "A randomly generated string used to autheticate between Lightstep and your AWS account. Default: <empty>"
   type    = string
 }
 
