994b684dd608acd73b55e32041de9ab08d3b34d7
Jonathan Kart <jkart@lightstep.com>
2022-06-03 11:45:36 -0400

    add comment on how to assume a role

--- base.tf
+++ base.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 3.42.0"
     }
   }
@@ -9,6 +9,11 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+
+  # If required, you can assume a role with sufficient permissions to create the metric stream
+  # assume_role {
+  #  role_arn = "arn:aws:iam::<ACCOUNTID>:role/<ROLE-NAME>"
+  # }
 }
 
 data "aws_caller_identity" "current" {}
